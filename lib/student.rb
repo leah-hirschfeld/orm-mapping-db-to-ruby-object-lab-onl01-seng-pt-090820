d45d41b707832f0aea5eac1a39b75ac79299e42d
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-10 00:48:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,7 +67,8 @@ class Student
 
   def self.students_below_12th_grade
     sql = "SELECT * FROM students WHERE grade < 12"
-    DB[:conn].execute(sql).map do |row|
+      DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
     end
   end
 
