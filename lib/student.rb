e7ca1c0de64d17d8b0fafe97c5c8fab761034be6
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-10 00:49:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,7 +79,8 @@ class Student
 
   def self.first_student_in_grade_10
     sql = "SELECT * FROM students WHERE grade == 10 LIMIT 1"
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
   end
 
   def self.all_students_in_grade_X(grade)
